5728f42f973d16c120afa8b5a05f037c03f3749a
RupeshArora24 <rupesh.arora303@gmail.com>
2023-06-23 19:45:16 +0530

    Gcd of two numbers without using list

--- DSA_Practice_Questions/GCD_of_two_numbers.py
+++ DSA_Practice_Questions/GCD_of_two_numbers.py
@@ -8,7 +8,12 @@ def gcd(m,n) :
     print(cf)
     return print(cf[-1])
 
+def gcd_notlist(m,n) :
+    for i in range(1,min(m,n)+1):
+        if(m%i)==0 and (n%i)==0:
+            crcf = i;
+    return print(crcf)
 
 gcd(30,25)
-
+gcd_notlist(30,25)
 
